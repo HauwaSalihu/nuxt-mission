cbe7372fb340c4f8bdbf70e6a1f895a753ac03f7
Hauwa Salihu <hauwasalih1010@gmail.com>
2021-06-05 00:09:37 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">nuxt-mission</h1>
+      <h1 class="title">nuxt mission</h1>
       <PlanetsList />
     </div>
   </div>
